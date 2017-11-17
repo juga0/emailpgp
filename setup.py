ecf484e75b17d6aadf8d694bb4d9960431b6a59e
juga0 <juga@riseup.net>
2017-11-17 06:06:01 +0000

    Create package dir and move module into it

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='emailpgp',
-    version='0.1.1',
+    version='0.1.2',
     description='Extend Python email.mime classes to add MIME'
     ' multipart/pgp-encrypted type messages.',
     long_description='',
