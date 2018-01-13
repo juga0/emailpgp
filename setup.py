1ad09067502000353a53abd1a22d9e60be3ef930
juga0 <juga@riseup.net>
2018-01-13 23:15:53 +0000

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='emailpgp',
-    version='0.2.0',
+    version='0.2.1',
     description='Extend Python email.mime classes to add MIME'
     ' multipart/pgp-encrypted type messages.',
     long_description='',
