a880663e61fc0e6e6fa8a6a5c40291b890c4423d
juga0 <juga@riseup.net>
2018-01-13 22:22:27 +0000

    Add python versions classifiers to setup

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Networking',
     ],
 )
